a7565fcc32b8266687fae1d4af1fe211ffb8bba2
Douglas Araújo <devdoug45@gmail.com>
2024-01-14 22:33:05 +0000

    feat: req06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    lower_index = 0
+    higher_index = len(word) - 1
+
+    while lower_index < higher_index:
+        if word[lower_index] != word[higher_index]:
+            return False
+        lower_index += 1
+        higher_index -= 1
+    return True

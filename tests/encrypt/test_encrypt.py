033ddf68d246c0ee10fdd7271897acea70cffef1
Douglas Araújo <devdoug45@gmail.com>
2024-01-14 16:46:53 +0000

    feat: req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
+    assert encrypt_message("text", 6) == "txet"
+
+    assert encrypt_message("text", 1) == "t_txe"
+
+    assert encrypt_message("text", 2) == "tx_et"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("text", '1')
     pass

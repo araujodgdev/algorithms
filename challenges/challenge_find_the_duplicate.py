400bd11d016fd28147761313c6fc68a0ba8f31f3
Douglas Araújo <devdoug45@gmail.com>
2024-01-14 22:29:47 +0000

    feat: req05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
+    if (
+        not nums
+        or len(nums) < 2
+        or not all(isinstance(item, int) for item in nums)
+    ):
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums)-1):
+        if nums[i] < 0:
+            return False
+
+        if nums[i] == nums[i+1]:
+            return nums[i]
+
+    return False
     raise NotImplementedError
